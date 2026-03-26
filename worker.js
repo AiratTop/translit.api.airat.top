4bdf0518ab99e83ad7c7db4ea8e589ddcd238e23
AiratTop <airat.halitov@gmail.com>
2026-03-26 21:04:42 +0300

    Format JSON

--- worker.js
+++ worker.js
@@ -93,7 +93,7 @@ async function readTextFromBody(request) {
 }
 
 function jsonResponse(data, status = 200) {
-  return new Response(JSON.stringify(data), {
+  return new Response(JSON.stringify(data, null, 2), {
     status,
     headers: {
       'Content-Type': 'application/json; charset=utf-8',
